b44f5222aeda557e18fd0242914d56e957a1bef9
ChaimShvadron <s0527649958@gmail.com>
2025-07-09 10:05:11 +0300

    Refactor load_data_csv and calculate_conditional_counts for improved data handling and clarity

--- naivBayes.py
+++ naivBayes.py
@@ -1,24 +1,40 @@
 import pandas as pd
 
 def load_data_csv(file_path):
-    return pd.read_csv(file_path)
+    data = pd.read_csv(file_path)
+    for col in data.columns:
+        if data[col].dtype == 'bool':
+            data[col] = data[col].astype(str)
+    return data
 
 def calculate_conditional_counts(data):
     target_column = data.columns[-1]
     features = data.columns[:-1]
-    classes = data[target_column].value_counts()   
+    classes = data[target_column].value_counts()
+
+    all_possible_values = {
+        feature: data[feature].unique()
+        for feature in features
+    }
+
     result = {}
+
     for class_name, class_count in classes.items():
-        class_subset = data[data[target_column] == class_name]     
-        if class_name not in result:
-            result[class_name] = {}
+        class_subset = data[data[target_column] == class_name]
+        result[class_name] = {}
+
         for feature in features:
-            value_counts = class_subset[feature].value_counts()
-            for value, count in value_counts.items():
-                if feature not in result[class_name]:
-                    result[class_name][feature] = {}
-                result[class_name][feature][value] = count / class_count
+            result[class_name][feature] = {}
+
+            values = all_possible_values[feature]
+            num_possible_values = len(values)
+
+            for value in values:
+                count = len(class_subset[class_subset[feature] == value])
+                result[class_name][feature][value] = (count + 1) / (class_count + num_possible_values)
+
     return result
+
 
 
 if __name__ == "__main__":
