f6e376db50da528925b62e4725fd8550b2dec0c5
ChaimShvadron <s0527649958@gmail.com>
2025-07-12 22:32:17 +0300

    Add random_state parameter to load_and_split_csv method for reproducible data sampling; update load_and_prepare_data to use new dataset

--- naive_bayes_oop/data_loader.py
+++ naive_bayes_oop/data_loader.py
@@ -9,10 +9,10 @@ class DataLoader:
                 data[col] = data[col].astype(str)
         return data
     
-    def load_and_split_csv(self, file_path, test_size=0.3):
+    def load_and_split_csv(self, file_path, test_size=0.3, random_state=None):
         data = self.load_data_csv(file_path)
         # sample test set
-        test = data.sample(frac=test_size)
+        test = data.sample(frac=test_size, random_state=random_state)
         # remaining as train set
         train = data.drop(test.index)
         return train, test
